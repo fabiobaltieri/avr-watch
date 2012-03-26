f2d7abe9da9e606705d28ce50bc40ab52b44ba1a
Fabio Baltieri <fabio.baltieri@gmail.com>
2012-03-26 20:07:06 +0200

    firmware: usbconfig: change product string

--- firmware/usbconfig.h
+++ firmware/usbconfig.h
@@ -252,8 +252,8 @@ section at the end of this file).
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'T', 'e', 's', 't', ' ', 'D', 'e', 'v', 'i', 'c', 'e'
-#define USB_CFG_DEVICE_NAME_LEN 11
+#define USB_CFG_DEVICE_NAME     'A', 'V', 'R', '-', 'C', 'l', 'o', 'c', 'k'
+#define USB_CFG_DEVICE_NAME_LEN 9
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
